6654538f561ffb4f07ceb0a8f5f82b8ee9bdbf15
Shawn Stubbs <sstubbs@streamflo.com>
2026-07-20 22:56:27 -0600

    Disable no-inferrable-types lint rule (fails --ship on stylistic annotations)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 module.exports = {
   extends: ['@microsoft/eslint-config-spfx/lib/profiles/default'],
   parserOptions: { tsconfigRootDir: __dirname },
+  rules: {
+    // Purely stylistic; we keep explicit annotations for readability.
+    '@typescript-eslint/no-inferrable-types': 'off'
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
